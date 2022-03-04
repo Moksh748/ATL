4f719d077bad643715676f1c451e39271b824ea7
Moksh748 <83644212+Moksh748@users.noreply.github.com>
2022-03-04 20:46:51 +0530

    Add files via upload

---
+++ Assignment.js
@@ -0,0 +1,6 @@
+const newfunction=()=>{
+    const div = document.createElement('p');
+    div.innerHTML="<ul><li class='Title'>Food:Pizza</li><li class='Title'>Colour:Green</li><li class='Title'>Sport:Cricket</li><li class='Title'>Season:Summer</li></ul>";
+    document.querySelector('body').appendChild(div);
+}
+
